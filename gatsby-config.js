a2eb792ba7a96638dc6a05a152623d9d7211bc76
Rodolfo Carvalho <rodolfo@rodolfo.tech>
2019-02-23 22:22:14 -0500

    removing extra spaces

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-favicon  `
+    `gatsby-plugin-favicon`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
